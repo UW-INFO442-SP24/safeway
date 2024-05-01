f7caf2a5d2c54a3175758195d866269439c8855d
stev_v <97136858+stevi2002@users.noreply.github.com>
2024-05-01 14:45:23 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import HomePage from './homepage';
-import EventPage from './components/EventPage/event_page';
-import LandingPage from './components/LandingPage/LandingPage';
-import ResourcePage from './components/ResourcePage/ResourcePage';
+import HomePage from './homepage'; // Adjusted path for HomePage
+import EventPage from './components/EventPage/event_page'; // Adjusted path for EventPage
+import ResourcePage from './components/ResourcePage/resource'; // Adjusted path for ResourcePage
 
 const App = () => {
     return (
@@ -13,14 +12,12 @@ const App = () => {
                     <ul>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/create-event">Create Event</Link></li>
-                        <li><Link to="/landing">Landing</Link></li>
                         <li><Link to="/resources">Resources</Link></li>
                     </ul>
                 </nav>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/create-event" element={<EventPage />} />
-                    <Route path="/landing" element={<LandingPage />} />
                     <Route path="/resources" element={<ResourcePage />} />
                 </Routes>
             </div>
