70045bd5265b4c9635c73eebb4218c228521b5fa
vickyzhihuang <zhihuang@uw.edu>
2024-05-02 13:40:10 -0700

    link all pages

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
 //Vicky
-import React from 'react';
-import Resource from './components/Resource/resource'; 
-
-function App() {
-  return (
-    <div className="App">
-      <Resource /> 
-    </div>
-  );
-}
-
-export default App;
-
-//Steven
 // import React from 'react';
-// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-// import HomePage from './components/homepage';
-// import EventPage from './components/EventPage/event_page';
-// // import ResourcePage from './components/ResourcePage/resource';
+// import Resource from './components/Resource/resource'; 
 
-// const App = () => {
-//     return (
-//         <Router>
-//             <div>
-//                 <nav>
-//                     <ul>
-//                         <li><Link to="/">Home</Link></li>
-//                         <li><Link to="/create-event">Create Event</Link></li>
-//                         {/* <li><Link to="/resources">Resources</Link></li> */}
-//                     </ul>
-//                 </nav>
-//                 <Routes>
-//                     <Route path="/" element={<HomePage />} />
-//                     <Route path="/create-event" element={<EventPage />} />
-//                     {/* <Route path="/resources" element={<ResourcePage />} /> */}
-//                 </Routes>
-//             </div>
-//         </Router>
-//     );
-// };
+// function App() {
+//   return (
+//     <div className="App">
+//       <Resource /> 
+//     </div>
+//   );
+// }
 
 // export default App;
+
+//Steven
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import HomePage from './components/homepage';
+import EventPage from './components/EventPage/event_page';
+import Resource from './components/Resource/resource'; 
+
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/create-event">Create Event</Link></li>
+                        <li><Link to="/resources">Resource</Link></li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/create-event" element={<EventPage />} />
+                    <Route path="/resources" element={<Resource />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
+
+export default App;
